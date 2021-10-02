2da6fc92aeaae3a456f2b960ad383727bf3d74d2
successJohn <successojohn@gmail.com>
2021-10-02 16:57:20 -0700

    Add project files.

---
+++ BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,72 @@
+﻿var dataTable;
+//const Swal = require('sweetalert2');
+
+$(document).ready(function (){
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $("#myTable").DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", width: "20%" },
+            { "data": "author", width: "20%" },
+            { "data": "isbn", width: "20%" },
+
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class= "text-center">
+                                <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
+                                        Edit
+                                </a>
+                                   &nbsp
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:70px;"
+                                    onclick = Delete('/api/book?id='+${data})>
+                                    Delete </a>
+                                     
+                            </div>`
+                }, "width": "40%"
+            }
+
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "One Deleted, You will not be able to recover ",
+        icon: "warning",
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajas({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        
+                        toastr.success(message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(message);
+                    }
+                }
+            })
+        }
+    } )
+}
+
+
+
